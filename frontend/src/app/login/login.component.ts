0a113adbb754bb75c3d06a9fb5ba1d91e5002d0c
palastiimre <palastiimre@gmail.com>
2022-11-13 11:02:18 +0100

    Added new UI to Login

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, Validators} from "@angular/forms";
+import {EmployeeService} from "../services/employee.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -7,8 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm = this.formBuilder.group({
+    email: [null, [Validators.required]],
+    password: [null, [Validators.required]],
+  });
+
+  constructor(private formBuilder: FormBuilder, private employeeService: EmployeeService,private router: Router) { }
 
   ngOnInit(): void {  }
+
+  login(){
+    let employeeLoginModel: Partial<any> = this.loginForm.value;
+
+    console.log(employeeLoginModel)
+
+    this.employeeService.loginEmployee(employeeLoginModel)
+      .subscribe({
+        next: (response) => {
+          console.log(response)
+        },
+        error: (error) => {
+          console.log(error)
+        },
+        complete: () => {
+          console.log('completed')
+          this.router.navigate(['/'])
+        }
+
+      })
+  }
+
 
 }
